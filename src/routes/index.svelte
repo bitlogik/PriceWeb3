e82cfa519924b43a1c3e88a6652fe300caafd76b
Antoine FERRON [BitLogiK] <antoine.ferron@bitlogik.fr>
2023-03-29 12:27:15 +0200

    Update tokens

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -30,11 +30,6 @@ const tokens = {
         "decimals": 18,
         "addr": "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270"
     },
-    "AVAX": {
-        "decimals": 18,
-        "addr": "0x2C89bbc92BD86F8075d1DEcc58C7F4E0107f286b"
-    
-    },
     "USDT": {
         "decimals": 6,
         "addr": "0xc2132d05d31c914a87c6611c10748aeb04b58e8f"
@@ -43,185 +38,185 @@ const tokens = {
         "decimals": 6,
         "addr": "0x2791bca1f2de4661ed88a30c99a7a9449aa84174"
     },
-    "LINK": {
+    "BNB": {
         "decimals": 18,
-        "addr": "0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39"
-    },
-    "CRO": {
-        "decimals": 8,
-        "addr": "0xada58df0f643d959c2a47c9d4d4c1a4defe3f11c"
+        "addr": "0x3BA4c387f786bFEE076A58914F5Bd38d668B42c3"
     },
     "BUSD": {
         "decimals": 18,
         "addr": "0xdab529f40e671a1d4bf91361c21bf9f0c9712ab7"
     },
-    "UNI": {
+    "SHIB": {
         "decimals": 18,
-        "addr": "0xb33eaad8d922b1083446dc23f610c2567fb5180f"
+        "addr": "0x6f8a06447ff6fcf75d803135a7de15ce88c1d4ec"
+    },
+    "AVAX": {
+        "decimals": 18,
+        "addr": "0x2C89bbc92BD86F8075d1DEcc58C7F4E0107f286b"
     },
     "DAI": {
         "decimals": 18,
         "addr": "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063"
     },
-    "THETA": {
+    "UNI": {
         "decimals": 18,
-        "addr": "0xb46e0ae620efd98516f49bb00263317096c114b2"
+        "addr": "0xb33eaad8d922b1083446dc23f610c2567fb5180f"
     },
-    "FTM": {
+    "LINK": {
         "decimals": 18,
-        "addr": "0xc9c1c1c20b3658f8787cc2fd702267791f224ce1"
-    },
-    "GRT": {
-        "decimals": 18,
-        "addr": "0x5fe2b58c013d7601147dcdd68c143a77499f5531"
-    },
-    "UST": {
-        "decimals": 18,
-        "addr": "0x692597b009d13c4049a947cab2239b7d6517875f"
-    },
-    "MANA": {
-        "decimals": 18,
-        "addr": "0xa1c57f48f0deb89f569dfbe6e2b7f46d33606fd4"
-    },
-    "AAVE": {
-        "decimals": 18,
-        "addr": "0xd6df932a45c0f255f85145f286ea0b292b21c90b"
-    },
-    "LRC": {
-        "decimals": 18,
-        "addr": "0x84e1670f61347cdaed56dcc736fb990fbb47ddc1"
-    },
-    "CHZ": {
-        "decimals": 18,
-        "addr": "0xf1938ce12400f9a761084e7a80d37e732a4da056"
-    },
-    "ENJ": {
-        "decimals": 18,
-        "addr": "0x7ec26842f195c852fa843bb9f6d8b583a274a157"
+        "addr": "0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39"
     },
     "LEO": {
         "decimals": 18,
         "addr": "0x06d02e9d62a13fc76bb229373fb3bbbd1101d2fc"
     },
-    "MKR": {
-        "decimals": 18,
-        "addr": "0x6f7C932e7684666C9fd1d44527765433e01fF61d"
-    },
-    "AMP": {
-        "decimals": 18,
-        "addr": "0x0621d647cecbfb64b79e44302c1933cb4f27054d"
-    },
-    "SAND": {
-        "decimals": 18,
-        "addr": "0xc6d54d2f624bc83815b49d9c2203b1330b841ca0"
-    },
-    "HOT": {
-        "decimals": 18,
-        "addr": "0x0c51f415cf478f8d08c246a6c6ee180c5dc3a012"
-    },
-    "COMP": {
-        "decimals": 18,
-        "addr": "0x8505b9d2254a7ae468c0e9dd10ccea3a837aef5c"
-    },
-    "SUSHI": {
-        "decimals": 18,
-        "addr": "0x0b3f868e0be5597d5db7feb59e1cadbb0fdda50a"
-    },
-    "IOTX": {
-        "decimals": 18,
-        "addr": "0xf6372cdb9c1d3674e83842e3800f2a62ac9f3c66"
-    },
-    "CEL": {
-        "decimals": 4,
-        "addr": "0xd85d1e945766fea5eda9103f918bd915fbca63e6"
-    },
-    "SNX": {
-        "decimals": 18,
-        "addr": "0x50b728d8d964fd00c2d0aad81718b71311fef68a"
-    },
-    "NEXO": {
-        "decimals": 18,
-        "addr": "0x41b3966b4ff7b427969ddf5da3627d6aeae9a48e"
-    },
-    "BAT": {
-        "decimals": 18,
-        "addr": "0x3cef98bb43d732e2f285ee605a8158cde967d219"
-    },
-    "BIFI": {
-        "decimals": 18,
-        "addr": "0xfbdd194376de19a88118e84e279b977f165d01b8"
-    },
-    "LPT": {
-        "decimals": 18,
-        "addr": "0x3962f4a0a0051dcce0be73a7e09cef5756736712"
-    },
-    "HT": {
-        "decimals": 18,
-        "addr": "0xfad65eb62a97ff5ed91b23afd039956aaca6e93b"
-    },
-    "OMG": {
-        "decimals": 18,
-        "addr": "0x62414d03084eeb269e18c970a21f45d2967f0170"
-    },
-    "CRV": {
-        "decimals": 18,
-        "addr": "0x172370d5cd63279efa6d502dab29171933a610af"
-    },
-    "YFI": {
-        "decimals": 18,
-        "addr": "0xda537104d6a5edd53c6fbba9a898708e465260b6"
-    },
     "TUSD": {
         "decimals": 18,
         "addr": "0x2e1ad108ff1d8c782fcbbb89aad783ac49586756"
     },
-    "TEL": {
-        "decimals": 2,
-        "addr": "0xdf7837de1f2fa4631d716cf2502f8b230f1dcc32"
-    },
-    "UMA": {
+    "LDO": {
         "decimals": 18,
-        "addr": "0x3066818837c5e6ed6601bd5a91b0762877a6b731"
+        "addr": "0xc3c7d422809852031b44ab29eec9f1eff2a58756"
     },
-    "renBTC": {
+    "CRO": {
         "decimals": 8,
-        "addr": "0xdbf31df14b66535af65aac99c32e9ea844e14501"
+        "addr": "0xada58df0f643d959c2a47c9d4d4c1a4defe3f11c"
     },
-    "BNT": {
+    "APE": {
         "decimals": 18,
-        "addr": "0xc26d47d5c33ac71ac5cf9f776d63ba292a4f7842"
+        "addr": "0xB7b31a6BC18e48888545CE79e83E06003bE70930"
     },
-    "ZRX": {
+    "GRT": {
         "decimals": 18,
-        "addr": "0x5559edb74751a0ede9dea4dc23aee72cca6be3d5"
+        "addr": "0x5fe2b58c013d7601147dcdd68c143a77499f5531"
     },
-    "PAX": {
+    "FTM": {
         "decimals": 18,
-        "addr": "0x6f3b3286fd86d8b47ec737ceb3d0d354cc657b3e"
+        "addr": "0xc9c1c1c20b3658f8787cc2fd702267791f224ce1"
+    },
+    "SAND": {
+        "decimals": 18,
+        "addr": "0xBbba073C31bF03b8ACf7c28EF0738DeCF3695683"
+    },
+    "MANA": {
+        "decimals": 18,
+        "addr": "0xa1c57f48f0deb89f569dfbe6e2b7f46d33606fd4"
     },
     "FRAX": {
         "decimals": 18,
         "addr": "0x45c32fa6df82ead1e2ef74d17b76547eddfaff89"
     },
-    "SRM": {
-        "decimals": 6,
-        "addr": "0x6bf2eb299e51fc5df30dec81d9445dde70e3f185"
-    },
-    "ELON": {
+    "AAVE": {
         "decimals": 18,
-        "addr": "0xe0339c80ffde91f3e20494df88d4206d86024cdf"
+        "addr": "0xd6df932a45c0f255f85145f286ea0b292b21c90b"
+    },
+    "THETA": {
+        "decimals": 18,
+        "addr": "0xb46e0ae620efd98516f49bb00263317096c114b2"
     },
     "RPL": {
         "decimals": 18,
         "addr": "0x7205705771547cf79201111b4bd8aaf29467b9ec"
     },
-    "XYO": {
+    "PAX": {
         "decimals": 18,
-        "addr": "0xd2507e7b5794179380673870d88b22f94da6abe0"
+        "addr": "0x6f3b3286fd86d8b47ec737ceb3d0d354cc657b3e"
+    },
+    "SNX": {
+        "decimals": 18,
+        "addr": "0x50b728d8d964fd00c2d0aad81718b71311fef68a"
+    },
+    "USDD": {
+        "decimals": 18,
+        "addr": "0xffa4d863c96e743a2e1513824ea006b8d0353c57"
+    },
+    "CRV": {
+        "decimals": 18,
+        "addr": "0x172370d5cd63279efa6d502dab29171933a610af"
+    },
+    "CHZ": {
+        "decimals": 18,
+        "addr": "0xf1938ce12400f9a761084e7a80d37e732a4da056"
+    },
+    "MKR": {
+        "decimals": 18,
+        "addr": "0x6f7C932e7684666C9fd1d44527765433e01fF61d"
+    },
+    "HT": {
+        "decimals": 18,
+        "addr": "0xfad65eb62a97ff5ed91b23afd039956aaca6e93b"
+    },
+    "FXS": {
+        "decimals": 18,
+        "addr": "0x1a3acf6d19267e2d3e7f898f42803e90c9219062"
+    },
+    "PAXG": {
+        "decimals": 18,
+        "addr": "0x553d3d295e0f695b9228246232edf400ed3560b5"
+    },
+    "AGIX": {
+        "decimals": 8,
+        "addr": "0x190eb8a183d22a4bdf278c6791b152228857c033"
+    },
+    "MASK": {
+        "decimals": 18,
+        "addr": "0x2b9e7ccdf0f4e5b24757c1e1a80e311e34cb10c7"
+    },
+    "LRC": {
+        "decimals": 18,
+        "addr": "0x84e1670f61347cdaed56dcc736fb990fbb47ddc1"
+    },
+    "RNDR": {
+        "decimals": 18,
+        "addr": "0x61299774020da444af134c82fa83e3810b309991"
     },
     "1INCH": {
         "decimals": 18,
         "addr": "0x9c2c5fd7b07e95ee044ddeba0e97a665f142394f"
+    },
+    "NEXO": {
+        "decimals": 18,
+        "addr": "0x41b3966b4ff7b427969ddf5da3627d6aeae9a48e"
+    },
+    "WOO": {
+        "decimals": 18,
+        "addr": "0x1b815d120b3ef02039ee11dc2d33de7aa4a8c603"
+    },
+    "GUSD": {
+        "decimals": 2,
+        "addr": "0xc8a94a3d3d2dabc3c1caffffdca6a7543c3e3e65"
+    },
+    "ENJ": {
+        "decimals": 18,
+        "addr": "0x7ec26842f195c852fa843bb9f6d8b583a274a157"
+    },
+    "FET": {
+        "decimals": 18,
+        "addr": "0x7583feddbcefa813dc18259940f76a02710a8905"
+    },
+    "BAT": {
+        "decimals": 18,
+        "addr": "0x3cef98bb43d732e2f285ee605a8158cde967d219"
+    },
+    "INJ": {
+        "decimals": 18,
+        "addr": "0x4e8dc2149eac3f3def36b1c281ea466338249371"
+    },
+    "HOT": {
+        "decimals": 18,
+        "addr": "0x0c51f415cf478f8d08c246a6c6ee180c5dc3a012"
+    },
+    "GNO": {
+        "decimals": 18,
+        "addr": "0x5ffd62d3c3ee2e81c00a7b9079fb248e7df024a8"
+    },
+    "COMP": {
+        "decimals": 18,
+        "addr": "0x8505b9d2254a7ae468c0e9dd10ccea3a837aef5c"
+    },
+    "YFI": {
+        "decimals": 18,
+        "addr": "0xda537104d6a5edd53c6fbba9a898708e465260b6"
     }
 };
 
